2ca6329312df3423cc7a5a06eec07fdef8f58ce6
Henrique Moreira <henriquefmoreira2@gmail.com>
2026-03-15 19:24:24 -0300

    feat: implementação inicial do mapa Leaflet para o site

---
+++ static/js/index.js
@@ -0,0 +1,93 @@
+// Inicializa o mapa focado em Midtown Manhattan
+const map = L.map('map', {
+    scrollWheelZoom: false,
+    smoothWheelZoom: true,
+    zoomControl: true
+}).setView([-23.5384907945773, -46.65394544656181], 17);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    subdomains: 'abcd',
+    maxZoom: 20
+}).addTo(map);
+
+// Modifica o link de Leaflet no canto inferior para que seja aberto em uma nova aba
+document.addEventListener('click', function (e) {
+    const link = e.target.closest('.leaflet-control-attribution a');
+    if (link) {
+        e.preventDefault();
+        window.open(link.href, '_blank', 'noopener,noreferrer');
+    }
+}, true);
+
+const overlay = document.getElementById('map-overlay');
+const mapDiv = document.getElementById('map');
+
+mapDiv.addEventListener('wheel', function(e) {
+    if (e.ctrlKey) {
+        e.preventDefault();
+        map.scrollWheelZoom.enable();
+        overlay.style.display = 'none';
+    } else {
+        map.scrollWheelZoom.disable();
+        overlay.style.display = 'flex';
+        
+        clearTimeout(window.overlayTimer);
+        window.overlayTimer = setTimeout(() => {
+            overlay.style.display = 'none';
+        }, 1500);
+    }
+}, { passive: false });
+
+const iconBlue = L.divIcon({
+    className: 'marker color-blue',
+    iconSize: [16, 16],
+    iconAnchor: [8, 8]
+});
+const iconGreen = L.divIcon({
+    className: 'marker color-green',
+    iconSize: [16, 16],
+    iconAnchor: [8, 8]
+});
+const iconPurple = L.divIcon({
+    className: 'marker color-purple',
+    iconSize: [16, 16],
+    iconAnchor: [8, 8]
+});
+
+const locais = [
+    {
+        nome: "Dona Elisa Restaurante",
+        coords: [-23.538264931051398, -46.653986235576866],
+        url: "https://www.timessquarenyc.org/"
+    },
+    {
+        nome: "Empire State Building",
+        coords: [-23.538810951797632, -46.654006728343425],
+        url: "https://www.esbnyc.com/pt"
+    },
+    {
+        nome: "Central Park (The Lake)",
+        coords: [40.7758, -73.9712],
+        url: "https://www.centralparknyc.org/"
+    },
+    {
+        nome: "Grand Central Terminal",
+        coords: [40.7527, -73.9772],
+        url: "https://www.grandcentralterminal.com/"
+    }
+];
+
+locais.forEach(local => {
+    const marker = L.marker(local.coords, { icon: iconStyle }).addTo(map);
+    
+    marker.bindPopup(`<b>${local.nome}</b><br>Clique para visitar.`);
+
+    marker.on('click', () => {
+        setTimeout(() => {
+            window.open(local.url, '_blank');
+        }, 300);
+    });
+});
+
+// Adiciona o controle de atribuição de volta de forma discreta
+L.control.attribution({ position: 'bottomright' }).addTo(map);
