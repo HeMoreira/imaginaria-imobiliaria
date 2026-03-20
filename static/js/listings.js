2f9dfe2f663b5c60f4db7a76ea22a0a742520286
Henrique Moreira <henriquefmoreira2@gmail.com>
2026-03-20 18:27:25 -0300

    fix: melhora geral no funcionamento das listagens de imóveis + mudanças no style.css

--- static/js/listings.js
+++ static/js/listings.js
@@ -1,21 +1,26 @@
 const buttons = document.querySelectorAll('.filter-btn');
 
 const slides = document.querySelector('.carousel-slides');
-const cards = document.querySelectorAll('.card');
 const btnEsq = document.querySelector('.carousel-btn--left');
 const btnDir = document.querySelector('.carousel-btn--right');
+const cards = document.querySelectorAll('.card');
+let visible_cards = document.querySelectorAll('.card[style="display: block;"]');
 
-let totalCards = (cards.length-1)/2 | 0;
+let totalCards = cards.length-3;
+let pot_slide = 0;
 let index = 0;
 let timer = 0;
 
+cards.forEach(card => {
+    card.style.display = "block";
+});
+
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        let filter = button.getAttribute('data-filter');
-        const cards = document.querySelectorAll('.card');
-
         buttons.forEach(btn => btn.classList.remove('active'));
         button.classList.add('active');
+        
+        let filter = button.getAttribute('data-filter');
 
         cards.forEach(card => {
             card_type = card.querySelector('.card-type');
@@ -29,11 +34,24 @@ buttons.forEach(button => {
         });
         index = 0;
         moverSlide();
-        totalCards = (document.querySelectorAll('.card[style="display: block;"]').length-1)/2 | 0;
     });
 });
+function updateSlidePower() {
+    visible_cards = document.querySelectorAll('.card[style="display: block;"]');
+    if (window.innerWidth <= 600) {
+        totalCards = visible_cards.length;
+        pot_slide = -index * 100.35;
+    } else if (window.innerWidth <= 900) {
+        totalCards = visible_cards.length-1;
+        pot_slide = -index * 51.5;
+    } else {
+        totalCards = visible_cards.length-2;
+        pot_slide = -index * 33.85;
+    }
+}
 function moverSlide() {
-    slides.style.transform = `translateX(${-index * 55}%)`;
+    updateSlidePower();
+    slides.style.transform = `translateX(${pot_slide}%)`;
     timer = 0;
 }
 btnDir.addEventListener('click', () => {
