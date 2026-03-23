f867655c80e11dafb46ebeebfa1ad0bed1eeaa56
Henrique Moreira <henriquefmoreira2@gmail.com>
2026-03-23 18:38:12 -0300

    feat: formulário de adição de imóveis com wtforms
    O formulário html sem validação e proteção contra crsf foi substituído por um novo formulário de wtf-flask, possuindo validação e estilização adequada.

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_login import LoginManager, login_required, login_user
 from models import db
 from models import Imovel, ImagensImovel, PlantasImovel, Admin
 from utils import salvar_imagens
-from forms import AdminForm
+from forms import AdminForm, ImovelForm
+from werkzeug.datastructures import CombinedMultiDict
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'incrediblekeythatwilldefinitelybechangedbeforerunningintoproduction'
@@ -14,8 +15,8 @@ db.init_app(app)
 
 @login_manager.user_loader
 def load_user(admin_id):
-    print(Admin.query.get(admin_id))
-    return Admin.query.get(admin_id)
+    print(db.session.get(Admin, int(admin_id)))
+    return db.session.get(Admin, int(admin_id))
 
 @app.route("/", methods=['GET'])
 def index():
@@ -39,116 +40,121 @@ def login():
 @app.route("/11_damin_k", methods=['POST', 'GET'])
 @login_required
 def admin():
-    if request.method == 'POST':
+    form = ImovelForm(CombinedMultiDict((request.form, request.files)))
+    if request.method == 'POST' and form.validate():
         lista_de_imagens = [
-            request.files.get('caminho_da_imagem_principal'),
-            request.files.get('caminho_da_imagem_de_fachada1'),
-            request.files.get('caminho_da_imagem_de_fachada2'),
-            request.files.get('caminho_da_imagem_de_area_comum1'),
-            request.files.get('caminho_da_imagem_de_area_comum2'),
-            request.files.get('caminho_da_imagem_de_area_comum3'),
-            request.files.get('caminho_da_imagem_de_area_comum4'),
-            request.files.get('caminho_da_imagem_de_area_comum5'),
-            request.files.get('caminho_da_imagem_de_area_comum6'),
-            request.files.get('caminho_da_imagem_de_area_comum7'),
-            request.files.get('caminho_da_imagem_de_area_comum8'),
-            request.files.get('caminho_da_imagem_de_area_comum9'),
-            request.files.get('caminho_da_imagem_de_area_comum10'),
+            form.caminho_da_imagem_principal.data,
+            form.caminho_da_imagem_de_fachada1.data,
+            form.caminho_da_imagem_de_fachada2.data,
+            form.caminho_da_imagem_de_area_comum1.data,
+            form.caminho_da_imagem_de_area_comum2.data,
+            form.caminho_da_imagem_de_area_comum3.data,
+            form.caminho_da_imagem_de_area_comum4.data,
+            form.caminho_da_imagem_de_area_comum5.data,
+            form.caminho_da_imagem_de_area_comum6.data,
+            form.caminho_da_imagem_de_area_comum7.data,
+            form.caminho_da_imagem_de_area_comum8.data,
+            form.caminho_da_imagem_de_area_comum9.data,
+            form.caminho_da_imagem_de_area_comum10.data,
         ]
         lista_de_plantas = [
-            request.files.get('caminho_da_imagem_da_planta1'),
-            request.files.get('caminho_da_imagem_da_planta2'),
-            request.files.get('caminho_da_imagem_da_planta3'),
-            request.files.get('caminho_da_imagem_da_planta4'),
-            request.files.get('caminho_da_imagem_da_planta5'),
-            request.files.get('caminho_da_imagem_da_planta6'),
-            request.files.get('caminho_da_imagem_da_planta7'),
-            request.files.get('caminho_da_imagem_da_planta8'),
-            request.files.get('caminho_da_imagem_da_planta9'),
-            request.files.get('caminho_da_imagem_da_planta10')
+            form.caminho_da_imagem_da_planta1.data,
+            form.caminho_da_imagem_da_planta2.data,
+            form.caminho_da_imagem_da_planta3.data,
+            form.caminho_da_imagem_da_planta4.data,
+            form.caminho_da_imagem_da_planta5.data,
+            form.caminho_da_imagem_da_planta6.data,
+            form.caminho_da_imagem_da_planta7.data,
+            form.caminho_da_imagem_da_planta8.data,
+            form.caminho_da_imagem_da_planta9.data,
+            form.caminho_da_imagem_da_planta10.data,
         ]
-        lista_de_caminhos_das_imagens = salvar_imagens('static/images/pictures', lista_de_imagens)
-        lista_de_caminhos_das_plantas = salvar_imagens('static/images/plants', lista_de_plantas)
+        lista_de_caminhos_das_imagens = salvar_imagens(lista_de_imagens)
+        lista_de_caminhos_das_plantas = salvar_imagens(lista_de_plantas)
 
         imagens_novo_imovel = ImagensImovel(
             caminho_da_imagem_principal=lista_de_caminhos_das_imagens[0],
             caminho_da_imagem_de_fachada1=lista_de_caminhos_das_imagens[1],
             caminho_da_imagem_de_fachada2=lista_de_caminhos_das_imagens[2],
             caminho_da_imagem_de_area_comum1=lista_de_caminhos_das_imagens[3],
-            descricao_da_area_comum1=request.form.get('descricao_da_area_comum1'),
+            descricao_da_area_comum1=form.descricao_da_area_comum1.data,
             caminho_da_imagem_de_area_comum2=lista_de_caminhos_das_imagens[4],
-            descricao_da_area_comum2=request.form.get('descricao_da_area_comum2'),
+            descricao_da_area_comum2=form.descricao_da_area_comum2.data,
             caminho_da_imagem_de_area_comum3=lista_de_caminhos_das_imagens[5],
-            descricao_da_area_comum3=request.form.get('descricao_da_area_comum3'),
+            descricao_da_area_comum3=form.descricao_da_area_comum3.data,
             caminho_da_imagem_de_area_comum4=lista_de_caminhos_das_imagens[6],
-            descricao_da_area_comum4=request.form.get('descricao_da_area_comum4'),
+            descricao_da_area_comum4=form.descricao_da_area_comum4.data,
             caminho_da_imagem_de_area_comum5=lista_de_caminhos_das_imagens[7],
-            descricao_da_area_comum5=request.form.get('descricao_da_area_comum5'),
+            descricao_da_area_comum5=form.descricao_da_area_comum5.data,
             caminho_da_imagem_de_area_comum6=lista_de_caminhos_das_imagens[8],
-            descricao_da_area_comum6=request.form.get('descricao_da_area_comum6'),
+            descricao_da_area_comum6=form.descricao_da_area_comum6.data,
             caminho_da_imagem_de_area_comum7=lista_de_caminhos_das_imagens[9],
-            descricao_da_area_comum7=request.form.get('descricao_da_area_comum7'),
+            descricao_da_area_comum7=form.descricao_da_area_comum7.data,
             caminho_da_imagem_de_area_comum8=lista_de_caminhos_das_imagens[10],
-            descricao_da_area_comum8=request.form.get('descricao_da_area_comum8'),
+            descricao_da_area_comum8=form.descricao_da_area_comum8.data,
             caminho_da_imagem_de_area_comum9=lista_de_caminhos_das_imagens[11],
-            descricao_da_area_comum9=request.form.get('descricao_da_area_comum9'),
+            descricao_da_area_comum9=form.descricao_da_area_comum9.data,
             caminho_da_imagem_de_area_comum10=lista_de_caminhos_das_imagens[12],
-            descricao_da_area_comum10=request.form.get('descricao_da_area_comum10')
+            descricao_da_area_comum10=form.descricao_da_area_comum10.data
         )
         plantas_novo_imovel = PlantasImovel(
             caminho_da_imagem_da_planta1=lista_de_caminhos_das_plantas[0],
-            descricao_da_planta1=request.form.get('descricao_da_planta1'),
+            descricao_da_planta1=form.descricao_da_planta1.data,
             caminho_da_imagem_da_planta2=lista_de_caminhos_das_plantas[1],
-            descricao_da_planta2=request.form.get('descricao_da_planta2'),
+            descricao_da_planta2=form.descricao_da_planta2.data,
             caminho_da_imagem_da_planta3=lista_de_caminhos_das_plantas[2],
-            descricao_da_planta3=request.form.get('descricao_da_planta3'),
+            descricao_da_planta3=form.descricao_da_planta3.data,
             caminho_da_imagem_da_planta4=lista_de_caminhos_das_plantas[3],
-            descricao_da_planta4=request.form.get('descricao_da_planta4'),
+            descricao_da_planta4=form.descricao_da_planta4.data,
             caminho_da_imagem_da_planta5=lista_de_caminhos_das_plantas[4],
-            descricao_da_planta5=request.form.get('descricao_da_planta5'),
+            descricao_da_planta5=form.descricao_da_planta5.data,
             caminho_da_imagem_da_planta6=lista_de_caminhos_das_plantas[5],
-            descricao_da_planta6=request.form.get('descricao_da_planta6'),
+            descricao_da_planta6=form.descricao_da_planta6.data,
             caminho_da_imagem_da_planta7=lista_de_caminhos_das_plantas[6],
-            descricao_da_planta7=request.form.get('descricao_da_planta7'),
+            descricao_da_planta7=form.descricao_da_planta7.data,
             caminho_da_imagem_da_planta8=lista_de_caminhos_das_plantas[7],
-            descricao_da_planta8=request.form.get('descricao_da_planta8'),
+            descricao_da_planta8=form.descricao_da_planta8.data,
             caminho_da_imagem_da_planta9=lista_de_caminhos_das_plantas[8],
-            descricao_da_planta9=request.form.get('descricao_da_planta9'),
+            descricao_da_planta9=form.descricao_da_planta9.data,
             caminho_da_imagem_da_planta10=lista_de_caminhos_das_plantas[9],
-            descricao_da_planta10=request.form.get('descricao_da_planta10')
+            descricao_da_planta10=form.descricao_da_planta10.data
         )
         novo_imovel = Imovel(
-            nome=request.form.get('nome'),
-            descricao=request.form.get('descricao'),
-            tipo_de_produto=request.form.get('tipo_de_produto'),
-            status=request.form.get('status'),
-            esta_visivel=bool(request.form.get('esta_visivel')),
-            preco_compra=float(request.form.get('preco_compra') or 0),
-            preco_aluguel=float(request.form.get('preco_aluguel') or 0),
-            condominio=float(request.form.get('condominio') or 0),
-            iptu=float(request.form.get('iptu') or 0),
-            menor_area_em_metros_quadrados=float(request.form.get('menor_area_em_metros_quadrados') or 0),
-            maior_area_em_metros_quadrados=float(request.form.get('maior_area_em_metros_quadrados') or 0),
-            menor_quantidade_de_dormitorios=int(request.form.get('menor_quantidade_de_dormitorios') or 0),
-            maior_quantidade_de_dormitorios=int(request.form.get('maior_quantidade_de_dormitorios') or 0),
-            cidade=request.form.get('cidade'),
-            bairro=request.form.get('bairro'),
-            endereco=request.form.get('endereco'),
-            cep=request.form.get('cep'),
+            nome=form.nome.data,
+            descricao=form.descricao.data,
+            tipo_de_produto=form.tipo_de_produto.data,
+            status=form.status.data,
+            esta_visivel=form.esta_visivel.data,
+            preco_compra=form.preco_compra.data,
+            preco_aluguel=form.preco_aluguel.data,
+            condominio=form.condominio.data,
+            iptu=form.iptu.data,
+            menor_area_em_metros_quadrados=form.menor_area_em_metros_quadrados.data,
+            maior_area_em_metros_quadrados=form.maior_area_em_metros_quadrados.data,
+            menor_quantidade_de_dormitorios=form.menor_quantidade_de_dormitorios.data,
+            maior_quantidade_de_dormitorios=form.maior_quantidade_de_dormitorios.data,
+            cidade=form.cidade.data,
+            bairro=form.bairro.data,
+            endereco=form.endereco.data,
+            cep=form.cep.data,
         )
         novo_imovel.imagens_do_produto.append(imagens_novo_imovel)
         novo_imovel.plantas_do_produto.append(plantas_novo_imovel)
         try:
+            print(novo_imovel)
             db.session.add(novo_imovel)
             db.session.commit()
             imoveis = Imovel.query.all()
-            return render_template('admin.html', mensagem="Imóvel adicionado com sucesso!", imoveis=imoveis)
+            return redirect(url_for('admin'))
         except Exception as e:
+            print(e, "\nfail\n")
             db.session.rollback()
-            return render_template('admin.html', mensagem=f"Erro ao adicionar imóvel: {str(e)}")
+            imoveis = Imovel.query.all()
+            return render_template('admin.html', mensagem="Erro ao adicionar imóvel", imoveis=imoveis, form=form)
     else:
+        print(f"Erros de validação: {form.errors}")
         imoveis = Imovel.query.all()
-        return render_template('admin.html', imoveis=imoveis)
+        return render_template('admin.html', imoveis=imoveis, form=form)
 
 if __name__ == "__main__":
     with app.app_context():
--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
-from models import db
 import os
 
-def salvar_imagens(caminho_base:str, lista_de_imagens:list):
+caminho_base = 'static/images'
+
+def salvar_imagens(lista_de_imagens:list):
     lista_de_caminhos_das_imagens = []
     if lista_de_imagens != []:
         for imagem in lista_de_imagens:
             if imagem is not None and imagem.filename != '':
                 caminho_da_nova_imagem = os.path.join(caminho_base, imagem.filename)
                 if os.path.exists(caminho_da_nova_imagem):
-                    print(f"Arquivo {imagem.filename} já existe. Renomeando para evitar sobrescrita.")
-                    caminho_da_nova_imagem = renomear_para_um_nome_unico(caminho_base, imagem.filename, 1)
+                    caminho_da_nova_imagem = renomear_para_um_nome_unico(imagem.filename, 1)
                 imagem.save(caminho_da_nova_imagem)
                 lista_de_caminhos_das_imagens.append(caminho_da_nova_imagem)
             else:
@@ -19,9 +19,8 @@ def salvar_imagens(caminho_base:str, lista_de_imagens:list):
         return ["imagem não encontrada"] * len(lista_de_imagens)
             
 
-def renomear_para_um_nome_unico(caminho_base:str, filename:str, contagem:int):
+def renomear_para_um_nome_unico(filename:str, contagem:int):
     new_filename = filename
-    print(".." + new_filename)
     if filename.endswith(".png"):
         new_filename = filename[:-4] + f"({contagem}).png"
     elif filename.endswith(".jpg"):
@@ -29,8 +28,7 @@ def renomear_para_um_nome_unico(caminho_base:str, filename:str, contagem:int):
     elif filename.endswith(".jpeg"):
         new_filename = filename[:-5] + f"({contagem}).jpeg"
     caminho_da_nova_imagem = os.path.join(caminho_base, new_filename)
-    print("ll" + new_filename)
     if os.path.exists(caminho_da_nova_imagem):
-        return renomear_para_um_nome_unico(caminho_base, filename, contagem + 1)
+        return renomear_para_um_nome_unico(filename, contagem + 1)
     else:
         return caminho_da_nova_imagem
