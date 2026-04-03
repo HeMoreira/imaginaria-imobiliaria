d60bc70cf37f0a4c95b631bcdc3ca6d1bceba703
Henrique Moreira <henriquefmoreira2@gmail.com>
2026-04-03 19:17:38 -0300

    security: mudanças importantes relacionadas a regra de 'nunca confie no frontend'

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 from datetime import datetime, timedelta
 from flask import current_app
+from werkzeug.security import safe_join
+
 from models import ImagemComDescricao, PlantaComDescricao
 
 failed_login_attempts = {}
@@ -8,10 +10,10 @@ ip_block_duration = timedelta(minutes=15)
 caminho_base = 'images'
 
 def deletar_imagens(lista_de_caminhos:list):
-    caminho_absoluto_da_pasta_images = os.path.join(current_app.root_path, 'static')
+    caminho_absoluto_da_pasta_images = safe_join(current_app.root_path, 'static')
     if lista_de_caminhos != []:
         for caminho in lista_de_caminhos:
-            caminho_absoluto_da_imagem = os.path.join(caminho_absoluto_da_pasta_images, caminho)
+            caminho_absoluto_da_imagem = safe_join(caminho_absoluto_da_pasta_images, caminho)
             if os.path.exists(caminho_absoluto_da_imagem):
                 os.remove(caminho_absoluto_da_imagem)
 
