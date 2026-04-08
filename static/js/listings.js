068c791ed15c001c88314577ced65c00dc7c7aac
Henrique Moreira <henriquefmoreira2@gmail.com>
2026-04-08 11:54:18 -0300

    fix: bug causado por mais de um carrossel em uma mesma página
    Anteriormente, se dois ou mais carrosseis fossem adicionados à mesma página, apenas o primeiro funcionaria. Agora, cada carrossel funciona de forma individual graças a criação de ids para cada um deles e um sistema de listas na lógica de cada um deles. Em contrapartida, agora é necessário adicionar o script para o arquivo js em cada html onde esse carrossel é adicionado, já que o DOM assume que todos os carrosseis já foram gerados, o que torna problemática a adição do script diretamente no partials _carrossel.html.

--- static/js/listings.js
+++ static/js/listings.js
@@ -1,72 +1,94 @@
-const buttons = document.querySelectorAll('.filter-btn');
+let carrossel_list = [{}];
+carrossel_list[0]["carrossel"] = document.querySelector('.imoveis.id1');
+if (document.querySelector('.imoveis.id2')) {
+    carrossel_list[1] = {};
+    carrossel_list[1]["carrossel"] = document.querySelector('.imoveis.id2');
+}
 
-const slides = document.querySelector('.carousel-slides');
-const btnEsq = document.querySelector('.carousel-btn--left');
-const btnDir = document.querySelector('.carousel-btn--right');
-const cards = document.querySelectorAll('.card');
-let visible_cards = document.querySelectorAll('.card[style="display: block;"]');
+for (let i = 0; i < carrossel_list.length; i++) {
+    carrossel_list[i]["buttons"] = carrossel_list[i]["carrossel"].querySelectorAll('.filter-btn');
+    carrossel_list[i]["cards"] = carrossel_list[i]["carrossel"].querySelectorAll('.card');
+    
+    carrossel_list[i]["slides"] = carrossel_list[i]["carrossel"].querySelector('.carousel-slides');
+    carrossel_list[i]["btnEsq"] = carrossel_list[i]["carrossel"].querySelector('.carousel-btn--left');
+    carrossel_list[i]["btnDir"] = carrossel_list[i]["carrossel"].querySelector('.carousel-btn--right');
+    carrossel_list[i]["visible_cards"] = carrossel_list[i]["carrossel"].querySelectorAll('.card[style="display: block;"]');
 
-let totalCards = cards.length-3;
-let pot_slide = 0;
-let index = 0;
-let timer = 0;
+    carrossel_list[i]["totalCards"] = carrossel_list[i]["cards"].length-3;
+    carrossel_list[i]["pot_slide"] = 0;
+    carrossel_list[i]["index"] = 0;
+    carrossel_list[i]["timer"] = 0;
+}
 
-cards.forEach(card => {
+let all_cards = document.querySelectorAll('.card');
+all_cards.forEach(card => {
     card.style.display = "block";
-    moverSlide()
+    for (let i = 0; i < carrossel_list.length; i++) {
+        moverSlide(i);
+    }
 });
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        buttons.forEach(btn => btn.classList.remove('active'));
-        button.classList.add('active');
-        
-        let filter = button.getAttribute('data-filter');
+function updateOnClickFilters(indice) {
+    carrossel_list[indice]["buttons"].forEach(button => {
+        button.addEventListener('click', () => {
+            carrossel_list[indice]["buttons"].forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+            
+            let filter = button.getAttribute('data-filter');
 
-        cards.forEach(card => {
-            card_type = card.querySelector('.card-type');
-            const category = card_type.getAttribute('data-category');
+            carrossel_list[indice]["cards"].forEach(card => {
+                card_type = card.querySelector('.card-type');
+                const category = card_type.getAttribute('data-category');
 
-            if (filter === "ALL" || category === filter) {
-                card.style.display = "block";
-            } else {
-                card.style.display = "none";
-            }
+                if (filter === "ALL" || category === filter) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
+            });
+            carrossel_list[indice]["index"] = 0;
+            moverSlide(indice);
         });
-        index = 0;
-        moverSlide();
     });
-});
-function updateSlidePower() {
-    visible_cards = document.querySelectorAll('.card[style="display: block;"]');
+}
+for (let i = 0; i < carrossel_list.length; i++) {
+    updateOnClickFilters(i);
+}
+
+function updateSlidePower(indice) {
+    const visible_cards = carrossel_list[indice]["carrossel"].querySelectorAll('.card[style="display: block;"]');
     if (window.innerWidth <= 600) {
-        totalCards = visible_cards.length;
-        pot_slide = -index * 100.35;
+        carrossel_list[indice]["totalCards"] = visible_cards.length;
+        carrossel_list[indice]["pot_slide"] = -carrossel_list[indice]["index"] * 100.35;
     } else if (window.innerWidth <= 900) {
-        totalCards = visible_cards.length-1;
-        pot_slide = -index * 51.5;
+        carrossel_list[indice]["totalCards"] = visible_cards.length-1;
+        carrossel_list[indice]["pot_slide"] = -carrossel_list[indice]["index"] * 51.5;
     } else {
-        totalCards = visible_cards.length-2;
-        pot_slide = -index * 33.85;
+        carrossel_list[indice]["totalCards"] = visible_cards.length-2;
+        carrossel_list[indice]["pot_slide"] = -carrossel_list[indice]["index"] * 33.85;
     }
 }
-function moverSlide() {
-    updateSlidePower();
-    slides.style.transform = `translateX(${pot_slide}%)`;
-    timer = 0;
+function moverSlide(indice) {
+    updateSlidePower(indice);
+    carrossel_list[indice]["slides"].style.transform = `translateX(${carrossel_list[indice]["pot_slide"]}%)`;
+    carrossel_list[indice]["timer"] = 0;
 }
-btnDir.addEventListener('click', () => {
-    index = (index + 1) % totalCards;
-    moverSlide();
-});
-btnEsq.addEventListener('click', () => {
-    index = (index - 1 + totalCards) % totalCards;
-    moverSlide();
-});
+for (let i = 0; i < carrossel_list.length; i++) {
+    carrossel_list[i]["btnDir"].addEventListener('click', () => {
+        carrossel_list[i]["index"] = (carrossel_list[i]["index"] + 1) % carrossel_list[i]["totalCards"];
+        moverSlide(i);
+    });
+    carrossel_list[i]["btnEsq"].addEventListener('click', () => {
+        carrossel_list[i]["index"] = (carrossel_list[i]["index"] - 1 + carrossel_list[i]["totalCards"]) % carrossel_list[i]["totalCards"];
+        moverSlide(i);
+    });
+}
 setInterval(() => {
-    timer++;
-    if(timer >= 5){
-        index = (index + 1) % totalCards;
-        moverSlide();
+    for (let i = 0; i < carrossel_list.length; i++) {
+        carrossel_list[i]["timer"]++;
+        if(carrossel_list[i]["timer"] >= 5){
+            carrossel_list[i]["index"] = (carrossel_list[i]["index"] + 1) % carrossel_list[i]["totalCards"];
+            moverSlide(i);
+        }
     }
 }, 1000);
